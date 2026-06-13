c76a2e7823a11e47a8ef78737f1882fed9583ba7
Mallikarjun S M <mallikarjunmarabasari@gmail.com>
2026-06-13 23:56:22 +0530

    Project Frontend core

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "./Login";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="app-root">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
